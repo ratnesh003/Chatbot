d3873c9aea96954d1c74951c4338a33127f8319b
Ratnesh Pasi <ratneshpasi03@gmail.com>
2025-07-15 16:46:01 +0530

    fix: Correct API rewrite rule in Vite configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         target: 'https://chatbot-5tek.onrender.com',  // your backend
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\api/, ''),
+        rewrite: (path) => path.replace(/api/, ''),
       },
     },
   },
